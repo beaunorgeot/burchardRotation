3ac228d577e95b289958fe520a5b007f5ee566e5
beaunorgeot <beaunorgeot@gmail.com>
2016-03-11 14:57:05 -0800

    order of library loading updated

--- 80_20/pr_pvalue/verified_pr_by_pvalue.R
+++ 80_20/pr_pvalue/verified_pr_by_pvalue.R
@@ -1,16 +1,19 @@
-setwd("/Users/beaunorgeot/burchardRotation/80_20/pr_pvalue")
-genotypes = read.table("../PR_top200_MAFp01.ALL.ind-PR-ALL.replaceNA.rIN", sep = " ",header = T,comment.char = "") 
+#local
+#setwd("/Users/beaunorgeot/burchardRotation/80_20/pr_pvalue")
+#server
+setwd("/media/BurchardRaid01/LabShare/Home/bnorgeot/Desktop/80_20/pr_by_pvalue")
+library(caret)
+library(dplyr)
+library(doMC)
+registerDoMC(cores = 54)
+#local
+#genotypes = read.table("../PR_top200_MAFp01.ALL.ind-PR-ALL.replaceNA.rIN", sep = " ",header = T,comment.char = "") 
+#server
+genotypes = read.table("/media/BurchardRaid01/LabShare/Home/cmak/wgs/joint/FINAL.2015-10-30/asso/plink_80pct/top_200/plink/PR_top200_MAFp01.ALL.ind-PR-ALL.replaceNA.rIN", sep = " ",header = T,comment.char = "") 
 genotypes = as.data.frame(lapply(genotypes, as.character), stringsAsFactors = T) %>% dplyr::mutate(personID = paste("X0",IID,sep="_")) %>% select(-IID)
 pvalues = read.table("../Result_pur_1482individuals_80prct_Clean.MAFp01_Top1000.top200.txt",header = T)
 load("../clinical_predFEV_train_pr.RData")
 load("../clinical_predFEV_test_pr.RData")
-
-
-library(caret)
-library(dplyr)
-library(tidyr)
-library(doMC)
-registerDoMC(cores = 4)
 
 genotypes_train = genotypes %>% filter(personID %in% clinical_predFEV_train_pr$personID)  
 genotypes_train = inner_join(genotypes_train,clinical_predFEV_train_pr) %>% select(-c(age,Sex,BMI))
